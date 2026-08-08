6f5bf9137a7e5007801d2b7c688d8943cc728661
myabc4411-oss <myabc4411@gmail.com>
2026-08-08 16:34:30 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'memo-v8';
+const CACHE_NAME = 'memo-v9';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
@@ -16,7 +16,10 @@ self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   if (event.request.url.includes('googleapis.com') ||
       event.request.url.includes('accounts.google.com') ||
-      event.request.url.includes('gstatic.com')) return;
+      event.request.url.includes('gstatic.com') ||
+      event.request.url.includes('firebaseapp.com') ||
+      event.request.url.includes('firebase') ||
+      event.request.url.includes('google.com')) return;
 
   event.respondWith(
     fetch(event.request).catch(() => caches.match(event.request))
